07296325f594c42ebd8b63f0b06a7f21650fba83
DIEGO ALEXANDER VALENCIA CALDER�N <diegovalencia313667@correo.itm.edu.co>
2023-05-30 10:10:52 -0500

    Update SeederDb for PublicationsAsync

--- SchoolPublications/SchoolPublications/DAL/SeederDb.cs
+++ SchoolPublications/SchoolPublications/DAL/SeederDb.cs
@@ -6,7 +6,7 @@ namespace SchoolPublications.DAL
 {
         public class SeederDb
         {
-            private readonly DatabaseContext _context;
+            private readonly DatabaseContext _context;          //Accesos y conexiones
             private readonly IUserHelper _userHelper;
             private readonly IAzureBlobHelper _azureBlobHelper;
 
@@ -19,7 +19,7 @@ namespace SchoolPublications.DAL
 
             public async Task SeederAsync()
             {
-                await _context.Database.EnsureCreatedAsync();
+                await _context.Database.EnsureCreatedAsync();       //Patrón de diseño para crear la base de datos
                 await PopulateRolesAsync();
                 await PopulateUserAsync("Steve", "Jobs", "admin@gmail.com", "3002323232","102030", "SteveJobs.png", UserType.Admin);
                 await PopulateUserAsync("Bill", "Gates", "bill_gates_admin@yopmail.com", "4005656656", "405060", "BillGates.png", UserType.User);
@@ -37,6 +37,40 @@ namespace SchoolPublications.DAL
                     _context.Categories.Add(new Category { Name = "Ropa interior", Description = "Tanguitas, narizonas" });
                     _context.Categories.Add(new Category { Name = "Gamers", Description = "PS5, XBOX SERIES" });
                     _context.Categories.Add(new Category { Name = "Mascotas", Description = "Concentrado, jabón para pulgas." });
+                }
+            }
+
+            private async Task PopulatePublicationsAsync()
+            {
+                if (!_context.Publications.Any())
+                {
+                _context.Publications.Add(new Publication
+                    {
+                        new Publication
+                        {
+                            Title = "Publicación 1",
+                            Content = "Contenido de la publicación 1",
+                            CreatedDate = DateTime.Now,
+                            Comments = new List<Comment>
+                            {
+                                new Comment { Text = "¡Gran publicación!", CreatedDate = DateTime.Now },
+                                new Comment { Text = "Me encanta este contenido.", CreatedDate = DateTime.Now }
+                            }
+                        },
+                        new Publication
+                        {
+                            Title = "Publicación 2",
+                            Content = "Contenido de la publicación 2",
+                            CreatedDate = DateTime.Now,
+                            Comments = new List<Comment>
+                            {
+                                new Comment { Text = "Muy interesante.", CreatedDate = DateTime.Now }
+                            }
+                        }
+                    });
+
+                    await _context.Publications.AddRangeAsync(publications);
+                    await _context.SaveChangesAsync();
                 }
             }
 
