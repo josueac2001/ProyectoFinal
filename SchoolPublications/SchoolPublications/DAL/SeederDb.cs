2805049eb0a7a246c16e18185c46bab9c50ce7dc
DIEGO ALEXANDER VALENCIA CALDER�N <diegovalencia313667@correo.itm.edu.co>
2023-05-30 22:20:38 -0500

    I enhance the SeederDb for Categories and Publications and i created folder to images Publications

--- SchoolPublications/SchoolPublications/DAL/SeederDb.cs
+++ SchoolPublications/SchoolPublications/DAL/SeederDb.cs
@@ -25,8 +25,10 @@ namespace SchoolPublications.DAL
                 await PopulateRolesAsync();
                 await PopulateUserAsync("Steve", "Jobs", "admin@gmail.com", "3002323232","102030", "SteveJobs.png", UserType.Admin);
                 await PopulateUserAsync("Bill", "Gates", "bill_gates_admin@yopmail.com", "4005656656", "405060", "BillGates.png", UserType.User);
+                await PopulateUserAsync("John", "Doe", "john.doe@example.com", "123456789", "123 Main St", "ABC123", "john-doe.png", UserType.User);
+                await PopulateUserAsync("Jane", "Smith", "jane.smith@example.com", "987654321", "456 Elm St", "XYZ789", "jane-smith.png", UserType.User);
 
-                await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
 
             }
 
@@ -34,52 +36,44 @@ namespace SchoolPublications.DAL
             {
                 if (!_context.Categories.Any())
                 {
-                    _context.Categories.Add(new Category { Name = "Romance", Description = "Elementos tech"});
-                    _context.Categories.Add(new Category { Name = "Acción", Description = "Detergente, jabón, etc." });
-                    _context.Categories.Add(new Category { Name = "Ropa interior", Description = "Tanguitas, narizonas" });
-                    _context.Categories.Add(new Category { Name = "Gamers", Description = "PS5, XBOX SERIES" });
-                    _context.Categories.Add(new Category { Name = "Mascotas", Description = "Concentrado, jabón para pulgas." });
+                    _context.Categories.Add(new Category { Name = "Novela", Description = "Historia ficticia" , CreatedDate = DateTime.Now });
+                    _context.Categories.Add(new Category { Name = "Terror", Description = "Miedo, angustia o suspenso" , CreatedDate = DateTime.Now });
+                    _context.Categories.Add(new Category { Name = "Acción", Description = "Combate, persecuciones y situaciones de alto riesgo" , CreatedDate = DateTime.Now });
+                    _context.Categories.Add(new Category { Name = "Drama", Description = "Conflictos emocionales y personales" , CreatedDate = DateTime.Now });
                 }
             }
 
             private async Task PopulatePublicationsAsync()
             {
+                if (!_context.Products.Any())
+                {
+                    await AddPublicationsAsync("Medias Grises", 270000M, 12F, new List<string>() { "Ropa Interior", "Calzado" }, new List<string>() { "Medias1.png" });
+                    await AddProductAsync("Medias Negras", 300000M, 12F, new List<string>() { "Ropa Interior", "Calzado" }, new List<string>() { "Medias2.png" });
+                    await AddProductAsync("TV Samsung OLED", 5000000M, 12F, new List<string>() { "Tecnología", "Gamers" }, new List<string>() { "TvOled.png", "TvOled2.png" });
+                    await AddProductAsync("Play Station 5", 5000000M, 12F, new List<string>() { "Gamers" }, new List<string>() { "PS5.png", "PS52.png" });
+                    await AddProductAsync("Bull Dog Francés", 10000000M, 12F, new List<string>() { "Mascotas" }, new List<string>() { "Frenchie1.png", "Frenchie2.png", "Frenchie3.png" });
+                    await AddProductAsync("Cepillo de dientes", 5000M, 12F, new List<string>() { "Implementos de Aseo" }, new List<string>() { "CepilloDientes.png" });
+                    await AddProductAsync("Crema dental Pro Alivio", 25000M, 12F, new List<string>() { "Implementos de Aseo" }, new List<string>() { "CremaDental1.png", "CremaDental2.png" });
+                }
+            }
+        private async Task PopulatePublicationsAsync()
+            {
                 if (!_context.Publications.Any())
                 {
-                _context.Publications.Add(new Publication
+                    _context.Publications.Add(new Publication
                     {
-                        new Publication
+                        Title = "Cien años de soledad",
+                        Content = "La historia épica de la familia Buendía a lo largo de varias generaciones, llena de amor, tragedia y realismo mágico.",
+                        CreatedDate = DateTime.Now,
+                        Comments = new List<Comment>()
                         {
-                            Title = "Publicación 1",
-                            Content = "Contenido de la publicación 1",
-                            CreatedDate = DateTime.Now,
-                            Comments = new List<Comment>
-                            {
-                                new Comment { Text = "¡Gran publicación!", CreatedDate = DateTime.Now },
-                                new Comment { Text = "Me encanta este contenido.", CreatedDate = DateTime.Now }
-                            }
-                        },
-                        new Publication
-                        {
-                            Title = "Publicación 2",
-                            Content = "Contenido de la publicación 2",
-                            CreatedDate = DateTime.Now,
-                            Comments = new List<Comment>
-                            {
-                                new Comment { Text = "Muy interesante.", CreatedDate = DateTime.Now }
-                            }
-                        },
-                        new Publication
-                        {
-                            Title = "Publicación 3",
-                            Content = "Contenido de la publicación 3",
-                            CreatedDate = DateTime.Now,
-                            Comments = new List<Comment>
-                            {
-                                new Comment { Text = "Grandioso, es el mejor contenido que he leído.", CreatedDate = DateTime.Now }
-                                new Comment { Text = "Que agradable texto.", CreatedDate = DateTime.Now }
-                                new Comment { Text = "Potaxio.", CreatedDate = DateTime.Now }
-                            }
+                            new Comment { Text = "¡Una novela fascinante!", CreatedDate = DateTime.Now },
+                            new Comment { Text = "La narrativa es increíble.", CreatedDate = DateTime.Now }
+                            new Comment { Text = "No tengo palabras para expresar lo increíble que esta novela.", CreatedDate = DateTime.Now }
+                            new Comment { Text = "LMe asombra el caracter de estos personajes", CreatedDate = DateTime.Now }
+                                 
+                        }
+                    }
                         }
                     });
 
